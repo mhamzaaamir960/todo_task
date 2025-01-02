edef908dbc9d5312eacf5180caa1bb3c261c5fc8
mhamzaaamir960 <mhamzaaamir960@gmail.com>
2025-01-02 13:17:37 +0500

    user profile route added

---
+++ src/app/api/user/profile/route.ts
@@ -0,0 +1,28 @@
+import { getDataFromToken } from "@/app/helpers/getDataFromToken";
+import { db } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  try {
+    const userId = await getDataFromToken(request);
+    const user = await db.user.findUnique({
+      where: { id: userId },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      },
+    });
+
+    if (!user) {
+      return NextResponse.json({ erroe: "User not found" }, { status: 404 });
+    }
+
+    return NextResponse.json({
+      message: "User found",
+      data: user,
+    });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 400 });
+  }
+}
---
+++ src/app/helpers/getDataFromToken.ts
@@ -0,0 +1,18 @@
+import { NextRequest, NextResponse } from "next/server";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+export const getDataFromToken = (request: NextRequest) => {
+  const token: string = request.cookies.get("token")?.value || "";
+  let decodedToken: string | JwtPayload;
+  try {
+    decodedToken = jwt.verify(token, process.env.TOKEN_SECRET!) as JwtPayload;
+    console.log(decodedToken);
+    if (!decodedToken || typeof decodedToken === "string") {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    return decodedToken.id;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+};
